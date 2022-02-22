4434fd83d757d401ab9c807054e6f6345bde3ce3
August Manniche Møller <august.manniche@gmail.com>
2022-02-22 08:49:22 +0100

    hej

--- week3/class_execise/student.py
+++ week3/class_execise/student.py
@@ -12,41 +12,21 @@ class Student():
     def get_avg_grade(self):
         avg_grades_list = []
         avg_grade = 0
-        for g in self.data_sheet.course:
+        for g in self.data_sheet.courses:
             avg_grades_list.append(g.grade)
             avg_grade += g.grade
         return avg_grade/len(avg_grades_list)
 
     def __str__(self):
-        return f"Student name: {self.name}. Gender: {self.gender}. Course name: {self.data_sheet.name}. Teacher: {self.data_sheet.teacher}. ETCS: {self.data_sheet.ETCS}. Classroom: {self.data_sheet.classroom}. Grade: {self.data_sheet.grade}. "
+        return f"Student name: {self.name}. Gender: {self.gender}. Course name: {self.data_sheet}."
 
-    def random_generate(self, course, numbers_of_students):
-        list_of_names = ["August","Peter","Bo","Jens"]
-        list_of_genders = ["Male","Female","Another"]
-        list_of_students = []
-
-        f = open('list_of_students.csv', 'w', newline='')
-        writer = csv.writer(f)
-
-        for student in range(numbers_of_students):
-            student = Student(random.choice(list_of_names), random.choice(course), random.choice(list_of_genders), "")
-            list_of_students.append(student)
-
-        writer.writerow(list_of_students) 
-        f.close()
-        return list_of_students
-
-    
 class Data_sheet():
     
-    def __init__(self, course):
-        self.course = course
+    def __init__(self, courses):
+        self.courses = courses
 
     def get_grades(self):
-        grades = [] 
-        for g in self.course:
-            grades.append(g.grade)     
-        return grades
+        return [g.grade for g in self.courses]
 
 class Course():
     
@@ -56,6 +36,31 @@ class Course():
         self.teacher = teacher
         self.ETCS = ETCS
         self.grade = grade
+
+    def __str__(self):
+        return f"Course name: {self.name}. Teacher: {self.teacher}. ETCS: {self.ETCS}. Classroom: {self.classroom}. Grade: {self.grade}."
+
+def random_generate(courses, numbers_of_students):
+    list_of_names = ["August","Peter","Bo","Jens"]
+    list_of_genders = ["Male","Female","Another"]
+    list_of_students = []
+
+    for student in range(numbers_of_students):
+        number_of_courses = random.randint(1,len(courses))
+        student = Student(random.choice(list_of_names), random.sample(courses,number_of_courses), random.choice(list_of_genders), "")
+        list_of_students.append(student)
+        
+    return list_of_students
+
+def write_students_to_csv(list_of_students):
+
+    with open('list_of_students.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=["Name", "Gender", "Course name","Course teacher", "ETCS","Classroom","Grade" ], )
+        writer.writeheader() 
+
+        for student in list_of_students:
+            for course in student.data_sheet:
+                writer.writerow({"Name":student.name,"Gender":student.gender,"Course name": course.name, "Course teacher": course.teacher, "ETCS": course.ETCS, "Classroom": course.classroom, "Grade": course.grade})
 
 if __name__ == '__main__':
     first_course = Course("August", "L1.05", "Thomas", 10, 12)
@@ -70,6 +75,6 @@ if __name__ == '__main__':
 
     print(sheet.get_grades())
     print(person.get_avg_grade())
-    print(person.random_generate(course_list,5))
-    
+    students = random_generate(course_list,5)
+    write_students_to_csv(students)
     
